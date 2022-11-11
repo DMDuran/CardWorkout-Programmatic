5adad8e366a55c9a7023add8026d77128c9d3b7e
DMDuran <92621818+DMDuran@users.noreply.github.com>
2022-11-10 20:22:49 -0600

    Starting a swift project doing programmatic. Deleted storyboard and attachments. Added functions for the buttons and titles.

---
+++ CardWorkout-Programmatic/CWButton.swift
@@ -0,0 +1,37 @@
+//
+//  CWButton.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Marco Duran on 11/10/22.
+//
+
+import UIKit
+
+class CWButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //This is like create a function that you pass parameters by
+    //Allowing the user to pass a background color and a string for title
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        setTitle(title, for: .normal)
+    }
+    
+    func configure () {
+        layer.cornerRadius = 8
+        titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
+        setTitleColor(.white, for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false   //This means for the button to use auto-layout
+        
+    }
+
+}
--- CardWorkout-Programmatic/CardSelectionVC.swift
+++ CardWorkout-Programmatic/CardSelectionVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CardSelectionVC.swift
 //  CardWorkout-Programmatic
 //
 //  Created by Marco Duran on 11/10/22.
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CardSelectionVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        view.backgroundColor = .brown
+        
+    }
+    
 
 }
-
